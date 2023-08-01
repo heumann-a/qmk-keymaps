7320db6c52cf518a97b7d835d8b897f66911ac8e
heumann-a <heumann.alexander@googlemail.com>
2023-08-01 17:28:34 +0200

    Move Username ADD new keymap

--- compile.sh
+++ compile.sh
@@ -9,7 +9,7 @@ KEYMAP="hexa"
 
 WORKING_DIR="$(pwd)"
 export QMK_FIRMWARE_HOME="${WORKING_DIR}/../qmk_firmware"
-QMK_USER="${WORKING_DIR}/user"
+QMK_USER="${WORKING_DIR}/hexa"
 
 
 #------------------------------------------
@@ -54,6 +54,14 @@ case ${KEYBOARD_FOLDERNAME} in
         KEYMAP_LINK="${QMK_FIRMWARE_HOME}/keyboards/splitkb/aurora/lily58/keymaps/${KEYMAP}"
         FILENAME="splitkb_aurora_lily58_rev1"
         cp -r "${WORKING_DIR}/lily58" "${KEYMAP_LINK}"
+        cp -r "${QMK_USER}" "${QMK_FIRMWARE_HOME}/users/${KEYMAP}"
+        ;;
+    "lily58-new" )
+        echo "> Keymap folder lily58-new will be used!"
+        KEYBOARD="splitkb/aurora/lily58/rev1"
+        KEYMAP_LINK="${QMK_FIRMWARE_HOME}/keyboards/splitkb/aurora/lily58/keymaps/${KEYMAP}"
+        FILENAME="splitkb_aurora_lily58_rev1"
+        cp -r "${WORKING_DIR}/lily58-new" "${KEYMAP_LINK}"
         cp -r "${QMK_USER}" "${QMK_FIRMWARE_HOME}/users/${KEYMAP}"
         ;;
     *)
---
+++ hexa/config.h
@@ -0,0 +1,29 @@
+
+
+// -- Disable Debug --
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif // !NO_DEBUG
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+#define NO_PRINT
+#endif // !NO_PRINT
+// END Disable Debug
+
+
+#ifdef OLED_ENABLE
+#   define OLED_TIMEOUT 10000
+#   define CUSTOM_OLED_TIMEOUT 15000
+#endif
+
+// Not yet available in `info.json`
+#ifdef RGB_MATRIX_ENABLE
+#   define RGB_MATRIX_MAXIMUM_BRIGHTNESS RGBLIGHT_LIMIT_VAL
+#   define RGB_MATRIX_LED_COUNT RGBLED_NUM
+#   define RGB_MATRIX_SPLIT RGBLED_SPLIT
+#   define SPLIT_TRANSPORT_MIRROR
+#endif
+
+
+
+// Own Defines
+#define COMBO_COUNT 3
