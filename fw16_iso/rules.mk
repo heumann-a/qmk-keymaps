1fa847e348d916da0f002cb1010de9a7a85a89b9
heuman-a <heuman-a>
2024-07-07 09:36:47 +0200

    Fix QMK Function Collision with Userspace FIX FW16 ISO Compilation

---
+++ fw16_iso/rules.mk
@@ -0,0 +1,3 @@
+# Keyboard specific defines overwrite userspace
+TAP_DANCE_ENABLE = no		# Enable Tab Dance for Tap-Hold features
+CAPS_WORD_ENABLE = yes		# Enable Caps Word 
--- hexa/keyrecords/keycodes.c
+++ hexa/keyrecords/keycodes.c
@@ -2,13 +2,15 @@
 #include "keycodes.h"
 
 // Add the behaviour of this new keycode
+__attribute__ ((weak))
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-
   switch (keycode) {
-
-        // Process other keycodes normally
-        default:
-            return true;
-  } 
-
+    default:
+      break;
+  }
+  return process_record_keymap(keycode, record);
 }
